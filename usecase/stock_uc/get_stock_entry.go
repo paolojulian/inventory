8c39d0c3fab9c4f5bd9930980a43672b6813e8e5
Paolo Vincent Julian <paolo.julian@accelo.com>
2025-05-31 08:57:51 +0800

    feat: convert to go routines

--- usecase/stock_uc/get_stock_entry.go
+++ usecase/stock_uc/get_stock_entry.go
@@ -3,6 +3,7 @@ package stock_uc
 import (
 	"context"
 
+	"golang.org/x/sync/errgroup"
 	"paolojulian.dev/inventory/domain/product"
 	"paolojulian.dev/inventory/domain/stock"
 	"paolojulian.dev/inventory/domain/user"
@@ -58,25 +59,49 @@ func (uc *GetStockEntryUseCase) Execute(ctx context.Context, stockEntryID string
 		return nil, err
 	}
 
-	productSummary, err := uc.productRepo.GetSummary(ctx, stockEntry.ProductID)
-	if err != nil {
-		return nil, err
-	}
+	var (
+		productSummary   *product.ProductSummary
+		warehouseSummary *warehouse.WarehouseSummary
+		userSummary      *user.UserSummary
+	)
 
-	warehouseSummary, err := uc.warehouseRepo.GetSummary(ctx, stockEntry.WarehouseID)
-	if err != nil {
-		return nil, err
-	}
+	g, ctx := errgroup.WithContext(ctx)
 
-	userSummary, err := uc.userRepo.GetSummary(ctx, stockEntry.UserID)
-	if err != nil {
+	g.Go(func() error {
+		summary, err := uc.productRepo.GetSummary(ctx, stockEntry.ProductID)
+		if err != nil {
+			return err
+		}
+		productSummary = &summary
+		return nil
+	})
+
+	g.Go(func() error {
+		summary, err := uc.warehouseRepo.GetSummary(ctx, stockEntry.WarehouseID)
+		if err != nil {
+			return err
+		}
+		warehouseSummary = &summary
+		return nil
+	})
+
+	g.Go(func() error {
+		summary, err := uc.userRepo.GetSummary(ctx, stockEntry.UserID)
+		if err != nil {
+			return err
+		}
+		userSummary = &summary
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
 		return nil, err
 	}
 
 	return &GetStockEntryOutput{
 		StockEntry: &stockEntry,
-		Product:    &productSummary,
-		Warehouse:  &warehouseSummary,
-		User:       &userSummary,
+		Product:    productSummary,
+		Warehouse:  warehouseSummary,
+		User:       userSummary,
 	}, nil
 }
